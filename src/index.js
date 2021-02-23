8eead82c91b9ede73fe9bea31d638f18794ff1df
kgsi <kuusai1998@gmail.com>
2021-02-23 18:42:00 +0900

    feat: fix prh path

--- src/index.js
+++ src/index.js
@@ -7,10 +7,14 @@ module.exports = {
     "ja-no-mixed-period": moduleInterop(
       require("textlint-rule-ja-no-mixed-period")
     ),
-    "prh-rules": require("./prh-rules"),
+    prh: moduleInterop(require("textlint-rule-prh")),
   },
   rulesConfig: {
     "ja-no-mixed-period": true,
-    "prh-rules": true,
+    prh: {
+      rulePaths: [
+        "node_modules/textlint-rule-preset-smarthr/dict/prh-rules.yml",
+      ],
+    },
   },
 };
